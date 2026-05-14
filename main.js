c12246e9f1a07d4e3808cd3a50c213c05ee7717f
TejasIsAmazing <tejasisamazing.ntl@gmail.com>
2026-05-14 19:46:18 +0530

    Implementation of frac, sqrt and attach

--- main.js
+++ main.js
@@ -120,10 +120,10 @@ class SpecialExpressions {
  */
 function special_to_div(spec_element) {
 	if (spec_element.name == "over") {
-		return attach(spec_element.data[1], undefined, undefined, undefined, undefined, spec_element.data[0], undefined, "0.5em")
+		return attach(toElement(spec_element.data[1]), undefined, undefined, undefined, undefined, toElement(spec_element.data[0]), undefined, "0.5em")
 	}
 	if (spec_element.name == "under") {
-		return attach(spec_element.data[1], undefined, undefined, undefined, undefined, undefined, spec_element.data[0], "0.5em")
+		return attach(toElement(spec_element.data[1]), undefined, undefined, undefined, undefined, undefined, toElement(spec_element.data[0]), "0.5em")
 	}
 	if (spec_element.name == "sqrt") {
 		// sqrt is a single pop function, so has one piece of data
@@ -131,40 +131,155 @@ function special_to_div(spec_element) {
 		/** @type {String | HTMLDivElement} */
 		let content = spec_element.data[0];
 
+
 		// Want a sqrt symbol like thing, ok whole size of div, and will border above
 		// Can use the unicode symbol itself, seems nice
 		// But I want it to scale with the div!
 		// Can use svg for that ig
+		// Will stick to text cause text is selectable
 
 
-		// Ok but what will be the border color? Forcing it black seems bad
-		
-		// Oh wait have not though about color at all!
-		// Ig probably default
 		let sub_div = document.createElement("span")
-		sub_div.appendChild(content)
-		sub_div.style.display = "inline flex"
+		sub_div.style.display = "inline-block"
 		sub_div.style.borderTop = "solid thin"
-		content.style.zoom = 0.9;
+		sub_div.classList.add("math-sqrt-content")
+		sub_div.style.zoom = 0.9;
+		sub_div.appendChild(content)
 
 
-		let out_div = document.createElement("span")
-		out_div.innerText = "√"
+		let out_div = document.createElement("span");
+		out_div.style.display = "inline-flex";
+		out_div.style.alignItems = "flex-end"; // makes the sqrt symbol start from bottom
+
+		let outer_sub_div = document.createElement("span");
+
+		outer_sub_div.textContent = "√"
+		// I can't get the size of the inner div rn, cause its computed later. 
+		// So will have to update the scale later, and hence marking it with class
+		outer_sub_div.classList.add("math-sqrt-symbol")
+		outer_sub_div.style.transformOrigin = "bottom center";
+		outer_sub_div.style.lineHeight = "1";
+		outer_sub_div.style.display = "inline-block";
+
 		
-		out_div.appendChild(sub_div)
+
+		out_div.appendChild(outer_sub_div);
+
+		out_div.appendChild(sub_div);
 		return out_div;
-
 		
-		throw Error("Not implemented sqrt yet")
 	}
 	if (spec_element.name == "cancel") {
 		throw Error("Not implemented cancel yet")
 	}
 	if (spec_element.name == "frac") {
-		throw Error("Not implemented fractions yet")
+		let numerator = toElement(spec_element.data[0]);
+		let denominator = toElement(spec_element.data[1]);
+		
+		let frac_div = document.createElement("span");
+		frac_div.style.display = "inline-flex";
+		frac_div.style.flexDirection = "column";
+		frac_div.style.alignItems = "center"; 
+		frac_div.style.verticalAlign = "middle"; // Makes the bar basically line up inline
+		frac_div.style.whiteSpace = "nowrap";
+		frac_div.style.lineHeight = "1";
+		frac_div.style.zoom = 0.8;
+		
+		// Numerator
+		let numSpan = document.createElement("span");
+		numSpan.style.display = "inline-block";
+		numSpan.style.paddingBottom = "0.05em";
+		numSpan.appendChild(numerator);
+		frac_div.appendChild(numSpan);
+		
+		// Fraction bar, separate instead of border on both numerator and denom cause i can adjust it more easily
+		let bar = document.createElement("span");
+		bar.style.display = "inline-block";
+		bar.style.width = "100%"; 
+		bar.style.borderTop = "solid 0.05em";
+		bar.style.margin = "0.05em 0";
+		frac_div.appendChild(bar);
+		
+
+		let denSpan = document.createElement("span");
+		denSpan.style.display = "inline-block";
+		denSpan.style.paddingTop = "0.05em";
+		denSpan.appendChild(denominator);
+		frac_div.appendChild(denSpan);
+		
+		return frac_div;
 	}
 	if (spec_element.name == "root") {
-		throw Error("Not implemented roots yet")
+		/** @type {String | HTMLDivElement} */
+		let content = spec_element.data[1];
+
+
+		// Want a sqrt symbol like thing, ok whole size of div, and will border above
+		// Can use the unicode symbol itself, seems nice
+		// But I want it to scale with the div!
+		// Can use svg for that ig
+		// Will stick to text cause text is selectable
+
+
+		let sub_div = document.createElement("span")
+		sub_div.style.display = "inline-block"
+		sub_div.style.borderTop = "solid thin"
+		sub_div.classList.add("math-sqrt-content")
+		sub_div.style.zoom = 0.9;
+		sub_div.appendChild(content)
+
+
+		let out_div = document.createElement("span");
+		out_div.style.display = "inline-flex";
+		out_div.style.alignItems = "flex-end"; // makes the sqrt symbol start from bottom
+
+		let outer_sub_div = document.createElement("span");
+
+		outer_sub_div.textContent = "√"
+		// I can't get the size of the inner div rn, cause its computed later. 
+		// So will have to update the scale later, and hence marking it with class
+		outer_sub_div.classList.add("math-sqrt-symbol")
+		outer_sub_div.style.transformOrigin = "bottom center";
+		outer_sub_div.style.lineHeight = "1";
+		outer_sub_div.style.display = "inline-block";
+
+		
+
+		out_div.appendChild(outer_sub_div);
+
+		out_div.appendChild(sub_div);
+
+		// Currently terrible implementation, will fix later
+		let upper_element = toElement(spec_element.data[0]);
+		upper_element.style.zoom = 0.7;
+
+		return attach(out_div, undefined, undefined, upper_element);
+	}
+	if (spec_element.name == "attach") {
+		return attach(
+			toElement(spec_element.data[0]),
+			toElement(spec_element.data[1]),
+			toElement(spec_element.data[2]),
+			toElement(spec_element.data[3]),
+			toElement(spec_element.data[4]),
+			toElement(spec_element.data[5]),
+			toElement(spec_element.data[6]),
+			"0.3em"
+		)
+		// throw Error("How did this even run?")
+	}
+	if (spec_element.name == "attacho") {
+		// The attacho is the same as attach, but with an extra parameter of overlap, which is used to control for too much distance, like for summations, use 0.3em
+		return attach(
+			toElement(spec_element.data[0]),
+			toElement(spec_element.data[1]),
+			toElement(spec_element.data[2]),
+			toElement(spec_element.data[3]),
+			toElement(spec_element.data[4]),
+			toElement(spec_element.data[5]),
+			toElement(spec_element.data[6]),
+			spec_element.data[7].innerText // The overlap parameter
+		)
 	}
 }
 
@@ -406,7 +521,9 @@ escape_word_list.push(...[
 	"under",
 	"cancel", // \cancel{} Makes a diagonal strikethrough the character/expression, like in ≠
 	"frac", // \frac{a}{b} is explicitly division, so a above b with a horizontal line between
-	"root"
+	"root",
+	"attach", // \attach{}{}{}{}{}{}{}
+	"attacho" // Like attach, but also additional parameter of overlap
 	// Question, how do i do multi line braces?
 	// Latex does it with \left and \right, so can prob do that.
 ])
@@ -417,7 +534,7 @@ escape_word_list.push(...[
 // Refers to functions, which pop the next argument behind them.
 var single_pop_list = ["sqrt", "cancel"]; 
 var double_pop_list = ["over","under","frac","root"];
-var custom_handling_list = ["sqrt"]; // For sqrt, i want it to be usable as a character and a function. SO if sqrt is followed by a string, tis character, if followed by a div, its function
+var custom_handling_list = ["attach", "attacho"]; // For sqrt, i want it to be usable as a character and a function. SO if sqrt is followed by a string, tis character, if followed by a div, its function
 
 // TODO: Need to support multi lines?
 
@@ -599,6 +716,35 @@ function tex_to_div(tokens) {
 								throw Error("No two elements to pop!")
 							}
 							element_index += 2;
+						} else if (custom_handling_list.includes(current_word)) {
+							if (current_word == "attach") {
+								
+								// Take in 6 different inputs
+								let data = [];
+								for (let i = 0; i < 7; i++) {
+									if (element_index + i < expr_length) {
+										data.push(expression_array[element_index + i])
+									} else {
+										data.push(undefined);
+									}
+								}
+								// console.log(data)
+								final_expression_array.push(new SpecialExpressions(current_word, data));
+								element_index += 7;
+							}
+							if (current_word == "attacho") {
+								// Take in 7 different inputs
+								let data = [];
+								for (let i = 0; i < 8; i++) {
+									if (element_index + i < expr_length) {
+										data.push(expression_array[element_index + i])
+									} else {
+										data.push(undefined);
+									}
+								}
+								final_expression_array.push(new SpecialExpressions(current_word, data));
+								element_index += 8;
+							}
 						} else {
 							let out_str = escape_word_map[current_word];
 							final_expression_array.push(out_str);
@@ -654,6 +800,41 @@ customElements.define("m-eq", class extends HTMLElement {
 
 		this.innerHTML = "";
 		this.appendChild(tex_to_div(tokens));
+
+
+
+		// AI CONTENT BELOW
+		// This observer thing updates the sqrt whenever the svg loads
+		const observer = new ResizeObserver(() => {
+			let symbols = Array.from(this.querySelectorAll(".math-sqrt-symbol")).reverse();
+
+			for (let sym of symbols) {
+				let cont = sym.nextElementSibling;
+				if (!cont || !cont.classList.contains("math-sqrt-content")) continue;
+
+				// 1. Temporarily clear the scale. 
+				// If we don't do this, getBoundingClientRect() will measure the ALREADY scaled height
+				// and the math will shrink back to 1 on the second run!
+				sym.style.transform = "none";
+
+				// 2. Measure the true heights
+				let contHeight = cont.getBoundingClientRect().height;
+				let symHeight = sym.getBoundingClientRect().height;
+
+				// 3. Apply the perfect scale
+				if (symHeight > 0) {
+					let scaleY = contHeight / symHeight * 0.95;
+					sym.style.transform = `scale(1, ${scaleY})`;
+				}
+			}
+		});
+
+		// Tell the observer to watch every single math content box in this equation.
+		let contents = this.querySelectorAll(".math-sqrt-content");
+		for (let cont of contents) {
+			observer.observe(cont);
+		}
+
 	}
 });
 
